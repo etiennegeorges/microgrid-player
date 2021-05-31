733a202c7b51c203624aad9bc2304f8b37d30710
Etienne Georges <etienne.georges@eleves.enpc.fr>
2021-05-31 09:40:03 +0200

    Version dernier run

--- player.py
+++ player.py
@@ -4,6 +4,12 @@ import numpy as np
 import pandas
 import os
 import pulp
+import sys
+
+sys.path.append(os.getcwd()+'/../../')
+# from common import parc
+# from linear_prog.pulp_utils import *
+# import common.charts
 
 random_lambda = np.random.rand(48)
 rho = 0.95
@@ -30,7 +36,7 @@ class Player:
     def compute_all_load(self):
         load = np.zeros(self.horizon)
         batterie = np.zeros(self.horizon)
-        moyenne = self.moyenne()
+        # moyenne = self.moyenne()
         # for time in range(self.horizon):
         # load[time] = self.compute_load(time)
 
@@ -51,35 +57,56 @@ class Player:
         # version 3
         lp = pulp.LpProblem("lp", pulp.LpMinimize)
         lp.setSolver()
-        variable = {}
+        variable0 = {}
+        variable1 = {}
+        variable2 = {}
+        variable3 = {}
         for t in range(self.horizon):
-            variable[t] = {}
             var_name = "battery_loadcharge" + str(t)
-            variable[t] = pulp.LpVariable(var_name, 0, self.Pmax)
+            variable0[t] = pulp.LpVariable(var_name, 0, self.Pmax)
 
             var_name = "battery_loaddecharge" + str(t)
-            variable[t] = pulp.LpVariable(var_name, 0, self.Pmax)
+            variable1[t] = pulp.LpVariable(var_name, 0, self.Pmax)
 
             var_name = "demande_totale" + str(t)
-            variable[t] = pulp.LpVariable(var_name, 0, None)
+            variable2[t] = pulp.LpVariable(var_name, 0, None)
 
             constraint_name = "stock positif" + str(t)
             lp += pulp.lpSum(
-                [self.rho_c * variable[k]["battery_loadcharge"] - self.rho_d * variable[k]["battery_loaddecharge"] for k
-                 in range(self.horizon)]) >= 0, constraint_name
+                [self.rho_c * variable0[k] - self.rho_d * variable1[k] for k in range(t)]) >= 0, constraint_name
 
-            constraint_name = "stock ne dépasse pas la capacite" + str(t)
+            constraint_name = "puissance < capacite" + str(t)
             lp += pulp.lpSum(
-                [self.rho_c * variable[s]["battery_loadcharge"] - (variable[s]["battery_loaddecharge"] / self.rho_d) for
-                 s in range(self.horizon)]) >= self.capa, constraint_name
+                [self.rho_c * variable0[s] - (variable1[s] * (1/self.rho_d)) for s in range(t)]) <= self.capa, constraint_name
 
-            constraint_name = "egalite entre la demande et l'offre"
-            lp += self.data[t] + variable[t]["battery_loadcharge"] + variable[t]["battery_loaddecharge"] == variable[t][
-                "demande"], constraint_name
+            constraint_name = "egalite offre demande" + str(t)
+            lp += self.data[t] + variable0[t] + variable1[t] == variable2[t], constraint_name
 
-        lp.setObjective(pulp.lpSum([self.prices[s] * variable[s]["demande_totale"] for s in range(self.horizon)]))
-        for i in range(self.horizon):
-            load[t] = variable[t]["demande_totale"]
+            var_name = "reste_batterie" + str(t)
+            variable3[t] = pulp.LpVariable(var_name, 0, None)
+
+        constraint_name = "stock initial nul"
+        lp += variable3[0]==0, constraint_name
+
+        constraint_name = "reste dans la batterie"
+        lp += variable3[self.horizon - 1] == self.delta_t * pulp.lpSum([variable0[t] * self.rho_c - variable1[s] *  (1/self.rho_d) for s in range(self.horizon)]), constraint_name
+
+        lp.setObjective(
+            pulp.lpSum([self.prices[s] * variable2[s] for s in range(self.horizon)]) - self.prices[self.horizon - 1] * variable3[self.horizon - 1])
+
+        lp.solve()
+        print(lp)
+        # variable2[0].value()
+        # model = Model(lp, variable0, variable1, variable2, variable3)
+        #
+        # solve(model)
+        # print(lp)
+        # results = getResultsModel(pb, model)
+
+        for i in range(0, self.horizon):
+            load[i] = variable2[i].value()
+        # load = variable2
+        print(load)
         return load
 
     def take_decision(self, time):
@@ -102,7 +129,28 @@ class Player:
         m /= self.horizon
         return m
 
+## Essai test fesabilité
+# my_df = pandas.read_csv("indus_cons_scenarios.csv", sep=";", decimal=".")
+# scenario_data1 = np.array(my_df)
+# scenario_data = np.zeros(48)
+# for i in range(1, 49):
+#     scenario_data[i - 1] = scenario_data1[i][3]
+#
+# p = Player()
+# p.set_scenario(scenario_data)
+#
+# prices_test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7,
+#                8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+# p.set_prices(prices_test)
+#
+# load = p.compute_all_load()
+# print(load)
 
+
+
+
+
+## Essai classique
 # my_df = pandas.read_csv(os.path.join(os.getcwd(), 'indus_cons_scenarios.csv'), sep=";", decimal=".")
 # scenario_data1 = np.array(my_df)
 # scenario_data = np.zeros(48)
@@ -110,10 +158,11 @@ class Player:
 #     scenario_data[i - 1] = scenario_data1[i][3]
 # print("Demande :")
 # print(scenario_data)
-# 
+#
 # P = Player()
 # P.set_scenario(scenario_data)
 # P.set_prices(random_lambda)
+#
 # load = P.compute_all_load()
 # print("Sortie :")
 # print(load)
